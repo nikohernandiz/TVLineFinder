d97d959405873cc6fb9a78911e42064f015d732f
nikohernandiz <nikohernandiz@gmail.com>
2016-12-14 08:48:43 -0800

    Update and rename bot.py to config.py

--- bot.py
+++
@@ -1,4 +0,0 @@
-#credentials for Reddit bot to login
-ag = "tvLineFinder Archer Version 0.3"
-username = "whoslineisthat"
-password = "1qazzaq1!QA2"
---
+++ config.py
@@ -0,0 +1,7 @@
+#credentials for Reddit bot to login
+agent = "tvLineFinder Archer Version 0.4"
+username = "whoselineisthat"
+password = "1qazzaq1!QA2"
+APP_ID = "sNJ0ODPHsl6sNw"
+APP_SECRET = "2d8tpkojQIQ5UwnGWMGfvP4ukk0"
+
